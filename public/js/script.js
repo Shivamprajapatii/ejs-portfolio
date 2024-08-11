0f7b99c148c33fba5e51b9137263ea4282c3a5c7
Shivam Prajapati <shivamprajapati4794@gmail.com>
2024-08-11 10:24:35 +0530

    Implement responsivness and many more features

--- public/js/script.js
+++ public/js/script.js
@@ -1,57 +1,62 @@
-// const dynamicText = document.querySelector("h3 span");
+const dynamicText1 = document.querySelector("h3 span");
 
-// const words = ["Full Stack Web Devloper","Coffe Lover","System Designer","Vloger"];
-// let wordIndex = 0;
-// let charIndex = 0;
-// let isDeleting = false;
-
-
-// const typeEffect = () => {
-//   const currentWord = words[wordIndex];
-//   const currentChar = currentWord.substring(0,charIndex);
-//   dynamicText.textContent = currentChar;
-
-//   if(!isDeleting  && charIndex < currentWord.length) {
-//     charIndex ++;
-//     setTimeout(typeEffect,60);
-//   } else if(isDeleting  && charIndex > 0) {
-//     charIndex --;
-//     setTimeout(typeEffect,100);
-//   } else {
-//     isDeleting = !isDeleting;
-//     wordIndex = !isDeleting ? (wordIndex + 1) % words.length : wordIndex; 
-//     setTimeout(typeEffect,300);
-//   }
-// } 
-
-// typeEffect();
+const words1 = ["Full Stack Web Devloper","Coffe Lover","System Designer","Vloger"];
+let wordIndex1 = 0;
+let charIndex1 = 0;
+let isDeleting1 = false;
 
 
+const typeEffec1 = () => {
+  const currentWor1 = words1[wordIndex1];
+  const currentChar1 = currentWor1.substring(0,charIndex1);
+  dynamicText1.textContent = currentChar1;
 
-const dynamicText = document.querySelector("h6 span");
-
-const wordss = ["#GitHub","#linkdIn ","#Instagram","#Facebook","#Twitter"];
-let wordIndex = 0;
-let charIndex = 0;
-let isDeleting = false;
-
-
-const typeEffect = () => {
-  const currentWord = wordss[wordIndex];
-  const currentChar = currentWord.substring(0,charIndex);
-  dynamicText.textContent = currentChar;
-
-  if(!isDeleting  && charIndex < currentWord.length) {
-    charIndex ++;
-    setTimeout(typeEffect,60);
-  } else if(isDeleting  && charIndex > 0) {
-    charIndex --;
-    setTimeout(typeEffect,100);
+  if(!isDeleting1  && charIndex1 < currentWor1.length) {
+    charIndex1 ++;
+    setTimeout(typeEffec1,60);
+  } else if(isDeleting1  && charIndex1 > 0) {
+    charIndex1 --;
+    setTimeout(typeEffec1,100);
   } else {
-    isDeleting = !isDeleting;
-    wordIndex = !isDeleting ? (wordIndex + 1) % wordss.length : wordIndex; 
-    setTimeout(typeEffect,300);
+    isDeleting1 = !isDeleting1;
+    wordIndex1 = !isDeleting1 ? (wordIndex1 + 1) % words1.length : wordIndex1; 
+    setTimeout(typeEffec1,400);
   }
 } 
 
-typeEffect();
+typeEffec1();
+
+
+// Foooter
+
+
+
+const dynamicText2 = document.querySelector("h6 span");
+
+const words2 = ["#GitHub","#linkdIn ","#Instagram","#Facebook","#Twitter"];
+let wordIndex2 = 0;
+let charIndex2 = 0;
+let isDeleting2 = false;
+
+
+const typeEffec2 = () => {
+  const currentWord2 = words2[wordIndex2];
+  const currentChar2 = currentWord2.substring(0,charIndex2);
+  dynamicText2.textContent = currentChar2;
+
+  if(!isDeleting2  && charIndex2 < currentWord2.length) {
+    charIndex2 ++;
+    setTimeout(typeEffec2,60);
+  } else if(isDeleting2  && charIndex2 > 0) {
+    charIndex2 --;
+    setTimeout(typeEffec2,300);
+  } else {
+    isDeleting2 = !isDeleting2;
+    wordIndex2 = !isDeleting2 ? (wordIndex2 + 1) % words2.length : wordIndex2; 
+    setTimeout(typeEffec2,700);
+  }
+} 
+
+typeEffec2();
+
+
